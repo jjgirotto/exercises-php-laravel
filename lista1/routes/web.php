aef559e3c9a1c247ead635c53054623a1528f5ad
Juliana Girotto <ads.jjgirotto@gmail.com>
2025-02-18 21:51:40 -0300

    lista 1 - ex 8-15

--- lista1/routes/web.php
+++ lista1/routes/web.php
@@ -89,3 +89,74 @@ Route::post('ex8resp', function(Request $request) {
     $resultado = $base ** $expoente;
     return view('ex8', compact('resultado'));
 });
+
+Route::get('/ex9', function () {
+    return view('ex9');
+});
+Route::post('ex9resp', function(Request $request) {
+    $metros = floatval($request->input('metros'));
+    $centimetros = $metros * 100;
+    return view('ex9', compact('centimetros'));
+});
+
+Route::get('/ex10', function () {
+    return view('ex10');
+});
+Route::post('ex10resp', function(Request $request) {
+    $km = floatval($request->input('km'));
+    $milhas = $km * 0.621371;
+    return view('ex10', compact('milhas'));
+});
+
+Route::get('/ex11', function () {
+    return view('ex11');
+});
+Route::post('ex11resp', function(Request $request) {
+    $altura = floatval($request->input('altura'));
+    $peso = floatval($request->input('peso'));
+    $imc = $peso / ($altura ** 2);
+    return view('ex11', compact('imc'));
+});
+
+Route::get('/ex12', function () {
+    return view('ex12');
+});
+Route::post('ex12resp', function(Request $request) {
+    $preco = floatval($request->input('preco'));
+    $desconto = floatval($request->input('desconto'));
+    $resultado = $preco - ($desconto/100 * $preco);
+    return view('ex12', compact('resultado'));
+});
+
+Route::get('/ex13', function () {
+    return view('ex13');
+});
+Route::post('ex13resp', function(Request $request) {
+    $capital = floatval($request->input('capital'));
+    $taxa = floatval($request->input('taxa'));
+    $periodo = intval($request->input('periodo'));
+    $juros = ($capital * $taxa/100 * $periodo);
+    return view('ex13', compact('juros'));
+});
+
+Route::get('/ex14', function () {
+    return view('ex14');
+});
+Route::post('ex14resp', function(Request $request) {
+    $capital = floatval($request->input('capital'));
+    $taxa = floatval($request->input('taxa'));
+    $periodo = intval($request->input('periodo'));
+    $montante = ($capital * (1 + ($taxa/100)) ** $periodo);
+    return view('ex14', compact('montante'));
+});
+
+Route::get('/ex15', function () {
+    return view('ex15');
+});
+Route::post('ex15resp', function(Request $request) {
+    $dias = intval($request->input('dias'));
+    $horas = $dias * 24;
+    $minutos = $horas * 60;
+    $segundos = $minutos * 60;
+    return view('ex15', compact('horas', 'minutos', 'segundos'));
+});
