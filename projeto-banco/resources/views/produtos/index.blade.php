d14f02dbd7569f2c7ff70172e17cccf4549ec843
Juliana Girotto <ads.jjgirotto@gmail.com>
2025-03-18 22:20:30 -0300

    feat: Produto views

--- projeto-banco/resources/views/produtos/index.blade.php
+++ projeto-banco/resources/views/produtos/index.blade.php
@@ -21,6 +21,29 @@
         </div>
     @endif
 
+    <table class="table table-hover table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome do Produto</th><th>Nome da Categoria</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($produtos as $p)
+                <tr>
+                    <td>{{ $p->id }}</td>
+                    <td>{{ $p->nome }}</td>
+                    <td>{{ $p->categoria->nome }}</td>
+                    <td>
+                        <a href="#" class="btn btn-warning">Editar</a>
+                        <a href="#" class="btn btn-info">Consultar</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
